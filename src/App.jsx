42a435eb4404b6ef9caba3ff1d7a241b83a54ff9
kristofferund <djdifee@gmail.com>
2019-09-19 14:39:17 +0200

    Structure site design with grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,48 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Header from '@organisms/Header/Header';
+import Input from '@atoms/Input/Input';
 
 function App() {
 	return (
 		<div className="app">
-			<header className="app-header">
-				<Header />
-			</header>
-			<main>
-				<aside>Look for stuff</aside>
-				<section>
-					<Switch>
-						<Route exact path="/">Welcome!</Route>
-						<Route path="/get-started">Get started</Route>
-						<Route path="/components">Components</Route>
-						<Route path="/templates">Templates</Route>
-					</Switch>
-				</section>
-			</main>
-			<footer>Footer</footer>
+			<div className="app-content">
+				<header className="app-header">
+					<Header />
+				</header>
+				<main>
+					<aside>
+						<Input label="Search for e.g tabs" />
+					</aside>
+					<section>
+						<Switch>
+							<Route exact path="/">
+								<h2>
+								Welcome!
+								</h2>
+							</Route>
+							<Route path="/get-started">
+								<h2>
+								Get started
+								</h2>
+							</Route>
+							<Route path="/components">
+								<h2>
+								Components
+								</h2>
+							</Route>
+							<Route path="/templates">
+								<h2>
+								Templates
+								</h2>
+							</Route>
+						</Switch>
+					</section>
+				</main>
+			</div>
+			<footer>Footer
+				<br /> footer
+			</footer>
 		</div>
 	);
 }

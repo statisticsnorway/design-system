0535806d60fe1eac50217e8142b8ebee2d557754
kristofferund <djdifee@gmail.com>
2019-09-27 15:45:27 +0200

    Start filling footer with page specific content

--- src/organisms/Footer/Footer.jsx
+++ src/organisms/Footer/Footer.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Link from '@atoms/Link/Link';
+import logo from '@public/ssb-logo-white.svg';
 
 const Footer = () => (
-	<div className="footer-wrapper">Footer</div>
+	<div className="footer-wrapper">
+		<div className="d-flex flex-column justify-content-between">
+			<div>
+				<img className="logo" src={logo} alt="logo" />
+				<p>
+					Offisiell statistikk om det norske
+					samfunnet siden 1876
+				</p>
+			</div>
+			<div>
+				<Link href="https://www.ssb.no/" text="Statistisk Sentralbyrå © 2019" />
+			</div>
+		</div>
+		<div>Some info</div>
+	</div>
 );
 
 Footer.defaultProps = {};

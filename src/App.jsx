f84c661e47584f09caf4db8eb20df3f6e80b32fb
kristofferund <djdifee@gmail.com>
2019-09-30 09:31:29 +0200

    Make footer customizable with child nodes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,40 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import Link from '@atoms/Link/Link';
 import Header from '@organisms/Header/Header';
 import Footer from '@organisms/Footer/Footer';
 import Components from '@pages/Components/Components';
 import GetStarted from '@pages/GetStarted/GetStarted';
 import Templates from '@pages/Templates/Templates';
 import Welcome from '@pages/Welcome/Welcome';
+import logo from '@public/ssb-logo-white.svg';
+import { Facebook, Linkedin, Rss, Twitter } from 'react-feather';
 
 const NoMatchPage = () => <h3>404 - Not found</h3>;
+const slogan = `
+Offisiell statistikk om det norske
+samfunnet siden 1876
+`;
+const socialLinks = [
+	{
+		link: 'https://www.facebook.com/statistisksentralbyra/',
+		logo: <Facebook />,
+	},
+	{
+		link: 'https://twitter.com/ssbnytt',
+		logo: <Twitter />,
+	},
+	{
+		link: 'https://www.linkedin.com/company/statistics-norway/',
+		logo: <Linkedin />,
+	},
+	{
+		link: 'https://www.ssb.no/informasjon/rss',
+		logo: <Rss />,
+	},
+];
+
+const footerLink = <Link href="https://www.ssb.no/" text="Statistisk Sentralbyrå © 2019" />;
 
 const App = () => (
 	<div className="app">
@@ -37,7 +64,9 @@ const App = () => (
 			</main>
 		</div>
 		<footer>
-			<Footer />
+			<Footer link={footerLink} logo={logo} slogan={slogan} socialLinks={socialLinks}>
+				{/* Insert mega menu */}
+			</Footer>
 		</footer>
 	</div>
 );
--- src/organisms/Footer/Footer.jsx
+++ src/organisms/Footer/Footer.jsx
@@ -1,28 +1,64 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Link from '@atoms/Link/Link';
-import logo from '@public/ssb-logo-white.svg';
+import Button from '@atoms/Button/Button';
+import { ArrowUp } from 'react-feather';
 
-const Footer = () => (
+const Footer = ({
+	children, link, logo, scrollToTop, slogan, socialLinks,
+}) => (
 	<div className="footer-wrapper">
-		<div className="d-flex flex-column justify-content-between">
-			<div>
-				<img className="logo" src={logo} alt="logo" />
-				<p>
-					Offisiell statistikk om det norske
-					samfunnet siden 1876
-				</p>
+		<div className="footer-grid-content">
+			<div className="d-flex flex-column justify-content-between">
+				<div>
+					<img className="logo" src={logo} alt="logo" />
+					<p>
+						{slogan && slogan}
+					</p>
+				</div>
 			</div>
-			<div>
-				<Link href="https://www.ssb.no/" text="Statistisk Sentralbyrå © 2019" />
+			<div>{children}</div>
+		</div>
+		<div className="d-flex flex-row justify-content-between">
+			{link && link}
+			<div className="social-links">
+				{socialLinks && socialLinks.map(so => (
+					<a
+						className="social-navigation"
+						href={so.link}
+						key={so.link}
+						rel="noopener noreferrer"
+						target="_blank"
+					>{so.logo}
+					</a>
+				))}
 			</div>
 		</div>
-		<div>Some info</div>
+		{scrollToTop && (
+			<div className="scroll-to-top">
+				<Button
+					onClick={() => window.scroll({ top: 0, behavior: 'smooth' })}
+					text="Til toppen"
+					icon={<ArrowUp />}
+				/>
+			</div>
+		)}
 	</div>
 );
 
 Footer.defaultProps = {};
 
-Footer.propTypes = {};
+Footer.propTypes = {
+	children: PropTypes.node,
+	link: PropTypes.node,
+	logo: PropTypes.node,
+	scrollToTop: PropTypes.bool,
+	slogan: PropTypes.string,
+	socialLinks: PropTypes.arrayOf(
+		PropTypes.shape({
+			link: PropTypes.string,
+			logo: PropTypes.node,
+		}),
+	),
+};
 
 export default Footer;

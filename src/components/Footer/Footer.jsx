44f307d1bd6f0d6b376bfed6932bbd71ef2f24c4
kristofferund <djdifee@gmail.com>
2019-12-06 14:46:48 +0100

    Make footer responsive

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
-import { Button } from '@statisticsnorway/ssb-component-library';
-import { ArrowUp } from 'react-feather';
-import logo from '@public/ssb-logo-white.svg';
+import { Button, Link, Text } from '@statisticsnorway/ssb-component-library';
+import { ArrowUp, GitHub, Mail } from 'react-feather';
 
 const Footer = () => (
 	<div className="footer-wrapper">
-		<div className="d-flex flex-row justify-content-between align-items-center">
-			<img className="logo" src={logo} alt="logo" />
-			<span>Statistisk Sentralbyrå © 2019</span>
-			<Button negative onClick={() => window.scroll({ top: 0, behavior: 'smooth' })}>
-				<ArrowUp size={18} />&nbsp;Til toppen
-			</Button>
+		<div className="footer-content">
+			<Text small negative className="copyright-mark">© Statistisk sentralbyrå, 2020</Text>
+			<div className="interactive-elements">
+				<Link href=" " icon={<Mail />} negative>Kontakt designasvarlig</Link>
+				<Link href=" " icon={<GitHub />} negative>Github</Link>
+				<Button negative onClick={() => window.scroll({ top: 0, behavior: 'smooth' })}>
+					<ArrowUp size={18} />&nbsp;Til toppen
+				</Button>
+			</div>
 		</div>
 	</div>
 );

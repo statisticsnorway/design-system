6b3369b463b2175a2baac8386ce530a4153c2734
Nordseth, Carina <Carina.Nordseth@ssb.no>
2019-12-09 14:05:56 +0100

    Updatet code example CheckboxInfo

--- src/pages/Components/ComponentsInfo/CheckboxInfo.jsx
+++ src/pages/Components/ComponentsInfo/CheckboxInfo.jsx
@@ -1,27 +1,6 @@
 import React, { useState } from 'react';
 import CodeSnippet from '@components/CodeSnippet/CodeSnippet';
-import { CheckboxGroup, Divider, LeadParagraph, Tabs, Title } from '@statisticsnorway/ssb-component-library';
-
-const leadParagraphText = `
-Avhukingsbokser gir brukeren muligheten til å velge flere valg i et utvalg. 
-Default visning for avhukingsbokser er at ingen valg er markert, og de ulike valgene bør stå i alfabetisk rekkefølge.
-`;
-{/*Check boxes provides the user with a range of options where the user may select any number of choices, one, multiple or zero.
-The default view for checkboxes has no options selected and the options provided should be listed in alphabetical order.*/}
-
-const overviewTextCheckbox = `
-Avhukingsbokser har 3 ulike statuser; ikke-avhuket (ikke valgt), hover (musepeker over den)  og avhuket (valgt). 
-Som ikke-valgt vil boksen være nøytral med en grå ramme. Når brukeren peker over en boks vil boksens ramme bli tykkere og grønn, 
-samtidig som en grønn bakgrunnsstripe vises på hele avhukingsboksens rad. Når en boks er valgt, vil boksen bli fylt og markert 
-med et avhukingsikon. 
-`;
-
-{/*Check boxes have 3 different states, alternating between unchecked, hover and checked.
-In the unchecked state the check boxes are neutral with a greyed border.
-As the user hovers over an unchecked box it transforms into a thicker border and turns green,
-at the same time a light green stripe will appear over the hovered check box row.
-When an option is checked by the user, the box will be filled and a check-icon will be displayed.*/}
-
+import { CheckboxGroup, Divider, LeadParagraph, Paragraph, Tabs, Title } from '@statisticsnorway/ssb-component-library';
 
 const tabItems = [
 	{
@@ -50,6 +29,7 @@ const codeExample = `
 <CheckboxGroup
 	header="Header"
 	onChange={() => callback}
+	orientation="column"
 	selectedValue="item2"
 	items={[
 		{ label: 'Checkbox 1', value: 'checkbox1' },
@@ -61,15 +41,22 @@ const codeExample = `
 
 const codeExampleHtml = `
 <div class="ssb-checkbox-group">
-  <div class="ssb-title no-margin"><h5>Title</h5></div>
-  <div class="ssb-checkbox">
-    <input type="checkbox" id="option1" value="option1" />
-    <label class="checkbox-label" for="option1">Insert label</label>
-  </div>
-  <div class="ssb-checkbox">
-    <input type="checkbox" id="option2" value="option2" />
-    <label class="checkbox-label" for="option2">Insert label</label>
-  </div>
+	<h6 class="ssb-title checkbox-group-header no-margin">Header</h6>
+	<!-- Use flex-row If horizontal layout -->
+		<div class="boxes flex-column">
+        <div class="ssb-checkbox">
+            <input tabindex="0" id="item1" type="checkbox" value="item1">
+            <label class="checkbox-label" for="item1">Item 1</label>
+        </div>
+        <div class="ssb-checkbox">
+            <input tabindex="0" id="item2" type="checkbox" value="item2">
+            <label class="checkbox-label" for="item2">Item 2</label>
+        </div>
+        <div class="ssb-checkbox">
+            <input tabindex="0" disabled="" id="item3" type="checkbox" value="item3">
+            <label class="checkbox-label" for="item3">Item 3</label>
+        </div>
+    </div>
 </div>
 `;
 
@@ -83,7 +70,8 @@ const CheckboxInfo = () => {
 		<div className="col-lg-12">
 			<Title size={1}>Checkbox</Title>
 			<LeadParagraph>
-				{leadParagraphText}
+				Avhukingsbokser gir brukeren muligheten til å velge flere valg i et utvalg.
+				Default visning for avhukingsbokser er at ingen valg er markert, og de ulike valgene bør stå i alfabetisk rekkefølge.
 			</LeadParagraph>
 			<Tabs activeOnInit={tabItems[0].path} items={tabItems} onClick={tabClicked} />
 			<Divider className="mb-4" />
@@ -92,7 +80,12 @@ const CheckboxInfo = () => {
 				<div className="row mb-4">
 					<Title size={2} className="col-lg-12">CheckboxGroup</Title>
 					<div className="col-lg-6">
-						<p>{overviewTextCheckbox}</p>
+						<Paragraph>
+							Avhukingsbokser har 3 ulike statuser; ikke-avhuket (ikke valgt), hover (musepeker over den)  og avhuket (valgt).
+							Som ikke-valgt vil boksen være nøytral med en grå ramme. Når brukeren peker over en boks vil boksens ramme bli tykkere og grønn,
+							samtidig som en grønn bakgrunnsstripe vises på hele avhukingsboksens rad. Når en boks er valgt, vil boksen bli fylt og markert
+							med et avhukingsikon.
+						</Paragraph>
 					</div>
 					<div className="col-lg-6">
 						<CheckboxGroup
@@ -139,6 +132,11 @@ const CheckboxInfo = () => {
 								<td><code>onChange</code></td>
 								<td>func</td>
 								<td>Callback function when a value is changed</td>
+							</tr>
+							<tr>
+								<td><code>orientation</code></td>
+								<td>column or row , default column</td>
+								<td>Vertical og horizontal layout</td>
 							</tr>
 							<tr>
 								<td><code>selectedValue</code></td>

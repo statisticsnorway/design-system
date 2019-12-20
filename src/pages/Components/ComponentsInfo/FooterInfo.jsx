a92f745495513af563b36d7c93fdff44fc28c5ff
kristofferlund <kristofferrlund@gmail.com>
2019-12-20 12:43:58 +0100

    Update to version 2.0.0

---
+++ src/pages/Components/ComponentsInfo/FooterInfo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const FooterInfo = ({}) => (
+	<div>FooterInfo</div>
+);
+
+FooterInfo.defaultProps = {};
+
+FooterInfo.propTypes = {};
+
+export default FooterInfo;

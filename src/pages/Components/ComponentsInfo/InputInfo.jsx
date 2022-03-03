106b60ca0f6ce991c1c0af9a74211f475e78acf2
johnnadeluy <johnnadel.salita@gmail.com>
2022-03-03 13:27:58 +0100

    Minor styling adjustment; add more space to the type row for the input prop table
    MIMIR-1125

--- src/pages/Components/ComponentsInfo/InputInfo.jsx
+++ src/pages/Components/ComponentsInfo/InputInfo.jsx
@@ -220,114 +220,114 @@ const InputInfo = () => {
 						<thead style={{ textAlign: 'left' }}>
 							<tr>
 								<th><Title size={3}>Prop name</Title></th>
-								<th><Title size={3}>Type</Title></th>
+								<th className="col-1"><Title size={3}>Type</Title></th>
 								<th><Title size={3}>Description</Title></th>
 							</tr>
 						</thead>
 						<tbody>
 							<tr>
 								<td><code>role</code></td>
-								<td>string</td>
+								<td className="col-1">string</td>
 								<td>Defines a string value that assigns a role type to the input element wrapper</td>
 							</tr>
 							<tr>
 								<td><code>ariaLabelWrapper</code></td>
-								<td>string</td>
+								<td className="col-1">string</td>
 								<td>Defines a string value that labels the input element wrapper</td>
 							</tr>
 							<tr>
 								<td><code>ariaLabel</code></td>
-								<td>string</td>
+								<td className="col-1">string</td>
 								<td>Defines a string value that labels input element, use if label is not visible</td>
 							</tr>
 							<tr>
 								<td><code>ariaLabelledBy</code></td>
-								<td>string</td>
+								<td className="col-1">string</td>
 								<td>The ariaLabelledBy string identifies the id to the element (or elements) that labels the element it is applied to.</td>
 							</tr>
 							<tr>
 								<td><code>ariaLabelSearchButton</code></td>
-								<td>string</td>
+								<td className="col-1">string</td>
 								<td>Defines a string value that labels button element</td>
 							</tr>
 							<tr>
 								<td><code>className</code></td>
-								<td>string</td>
+								<td className="col-1">string</td>
 								<td>Optional container class</td>
 							</tr>
 							<tr>
 								<td><code>disabled</code></td>
-								<td>bool</td>
+								<td className="col-1">bool</td>
 								<td>Toggles disabled state</td>
 							</tr>
 							<tr>
 								<td><code>error</code></td>
-								<td>bool</td>
+								<td className="col-1">bool</td>
 								<td>Changes design</td>
 							</tr>
 							<tr>
 								<td><code>errorMessage</code></td>
-								<td>string</td>
+								<td className="col-1">string</td>
 								<td>Renders an error message underneath input field</td>
 							</tr>
 							<tr>
 								<td><code>handleChange</code></td>
-								<td>func</td>
+								<td className="col-1">func</td>
 								<td>Callback function</td>
 							</tr>
 							<tr>
 								<td><code>onFocus</code></td>
-								<td>func</td>
+								<td className="col-1">func</td>
 								<td>Callback on focus</td>
 							</tr>
 							<tr>
 								<td><code>onBlur</code></td>
-								<td>func</td>
+								<td className="col-1">func</td>
 								<td>Callback on blur</td>
 							</tr>
 							<tr>
 								<td><code>id</code></td>
-								<td>string</td>
+								<td className="col-1">string</td>
 								<td>ID to connect label with input field</td>
 							</tr>
 							<tr>
 								<td><code>label</code></td>
-								<td>string</td>
+								<td className="col-1">string</td>
 								<td>Label text</td>
 							</tr>
 							<tr>
 								<td><code>negative</code></td>
-								<td>bool</td>
+								<td className="col-1">bool</td>
 								<td>Changes design</td>
 							</tr>
 							<tr>
 								<td><code>placeholder</code></td>
-								<td>string</td>
+								<td className="col-1">string</td>
 								<td>Placeholder text</td>
 							</tr>
 							<tr>
 								<td><code>searchField</code></td>
-								<td>bool</td>
+								<td className="col-1">bool</td>
 								<td>Ads search icon with click</td>
 							</tr>
 							<tr>
 								<td><code>size</code></td>
-								<td>string</td>
+								<td className="col-1">string</td>
 								<td>Set inputfield size. lg is the only available option</td>
 							</tr>
 							<tr>
 								<td><code>submitCallback</code></td>
-								<td>func</td>
+								<td className="col-1">func</td>
 								<td>Callback for onSubmit</td>
 							</tr>
 							<tr>
 								<td><code>type</code></td>
-								<td>string</td>
+								<td className="col-1">string</td>
 								<td>Changes input type</td>
 							</tr>
 							<tr>
 								<td><code>value</code></td>
-								<td>string</td>
+								<td className="col-1">string</td>
 								<td>Input value. Can be initiated with a value</td>
 							</tr>
 						</tbody>

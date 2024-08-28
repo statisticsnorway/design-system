b39bb4522361872c9cf87e2d20f02b8e984795f6
Carl-OW <142233642+Carl-OW@users.noreply.github.com>
2024-08-28 14:30:44 +0200

    Lint and duplicate error fixes

--- src/pages/Components/ComponentsInfo/TableInfo.jsx
+++ src/pages/Components/ComponentsInfo/TableInfo.jsx
@@ -15,26 +15,124 @@ import {
 } from '@statisticsnorway/ssb-component-library';
 
 const tabItems = [
-	{
-		title: 'Oversikt',
-		path: '/overview',
-	},
-	{
-		title: 'Props',
-		path: '/props',
-	},
+	{ title: 'Oversikt', path: '/overview' },
+	{ title: 'Props', path: '/props' },
 ];
 
 const tabCode = [
-	{
-		title: 'React',
-		path: '/react',
-	},
-	{
-		title: 'Html',
-		path: '/html',
-	},
+	{ title: 'React', path: '/react' },
+	{ title: 'Html', path: '/html' },
 ];
+
+const overviewText = 'Rad- og kolonneoverskrifter er essensielle. En celle er en overskrift hvis den beskriver innholdet i cellene under eller ved siden av. Juster tall for enklere sammenligning.';
+
+const Statistics = () => (
+	<SSBTable caption="Antall besøk og årsverk på museer og samlinger">
+		<TableHead>
+			<TableRow>
+				<TableCell type="th" colSpan={1} />
+				<TableCell type="th" colSpan={3} align="center">
+					Antall
+				</TableCell>
+			</TableRow>
+			<TableRow>
+				<TableCell type="th">Museer og samlinger totalt</TableCell>
+				<TableCell type="th" align="center">
+					2020
+				</TableCell>
+				<TableCell type="th" align="center">
+					2021
+				</TableCell>
+				<TableCell type="th" align="center">
+					2022
+				</TableCell>
+			</TableRow>
+		</TableHead>
+		<TableBody>
+			<TableRow>
+				<TableCell type="th" scope="row">
+					Museer og samlinger totalt<sup>1</sup>
+				</TableCell>
+				<TableCell align="center">102</TableCell>
+				<TableCell align="center">101</TableCell>
+				<TableCell align="center">104</TableCell>
+			</TableRow>
+			<TableRow>
+				<TableCell type="th" scope="row" indentationLevel={1}>
+					Besøk totalt
+				</TableCell>
+				<TableCell align="center" style={{ fontWeight: 'bold' }}>
+					5 134 293
+				</TableCell>
+				<TableCell align="center" style={{ fontWeight: 'bold' }}>
+					6 485 173
+				</TableCell>
+				<TableCell align="center" style={{ fontWeight: 'bold' }}>
+					10 321 374
+				</TableCell>
+			</TableRow>
+			<TableRow>
+				<TableCell type="th" scope="row" indentationLevel={2}>
+					Enkeltbesøk
+				</TableCell>
+				<TableCell align="center">4 421 942</TableCell>
+				<TableCell align="center">5 474 120</TableCell>
+				<TableCell align="center">8 491 379</TableCell>
+			</TableRow>
+			<TableRow>
+				<TableCell type="th" scope="row" indentationLevel={2}>
+					Gruppebesøk
+				</TableCell>
+				<TableCell align="center">712 351</TableCell>
+				<TableCell align="center">1 011 053</TableCell>
+				<TableCell align="center">1 829 995</TableCell>
+			</TableRow>
+			<TableRow>
+				<TableCell type="th" scope="row" indentationLevel={2}>
+					Betalende besøkende
+				</TableCell>
+				<TableCell align="center">2 391 962</TableCell>
+				<TableCell align="center">2 774 376</TableCell>
+				<TableCell align="center">5 654 919</TableCell>
+			</TableRow>
+			<TableRow>
+				<TableCell type="th" scope="row" indentationLevel={1}>
+					Årsverk
+				</TableCell>
+				<TableCell align="center" />
+				<TableCell align="center" />
+				<TableCell align="center" />
+			</TableRow>
+			<TableRow>
+				<TableCell type="th" scope="row" indentationLevel={2}>
+					Lønte
+				</TableCell>
+				<TableCell align="center">4 173</TableCell>
+				<TableCell align="center">4 404</TableCell>
+				<TableCell align="center">4 645</TableCell>
+			</TableRow>
+			<TableRow>
+				<TableCell type="th" scope="row" indentationLevel={2}>
+					Faste stillinger
+				</TableCell>
+				<TableCell align="center">3 359</TableCell>
+				<TableCell align="center">3 516</TableCell>
+				<TableCell align="center">3 683</TableCell>
+			</TableRow>
+		</TableBody>
+		<TableFooter>
+			<TableRow>
+				<TableCell type="td" colSpan={4}>
+					<div>
+						<sup>1</sup> Tallet på museer vil variere fra år til år. For
+						eksempel leverer noen museer tall til statistikken ett år, men ikke
+						et annet.
+					</div>
+				</TableCell>
+			</TableRow>
+		</TableFooter>
+	</SSBTable>
+);
 
 const codeReact = `
 import { Table, TableHead, TableBody, TableFooter, TableRow, TableCell } from "@statisticsnorway/ssb-component-library";
@@ -69,151 +167,43 @@ export default SimpleTable;
 `;
 
 const codeHtml = `
-  <Table>
-    <TableHead>
-      <TableRow>
-        <TableCell type="th">Header 1</TableCell>
-        <TableCell type="th">Header 2</TableCell>
-      </TableRow>
-    </TableHead>
-    <TableBody>
-      <TableRow>
-        <TableCell>Data 1</TableCell>
-        <TableCell>Data 2</TableCell>
-      </TableRow>
-      <TableRow>
-        <TableCell>More Data 1</TableCell>
-        <TableCell>More Data 2</TableCell>
-      </TableRow>
-    </TableBody>
-    <TableFooter>
-      <TableRow>
-        <TableCell colSpan={2}>Footer Information</TableCell>
-      </TableRow>
-    </TableFooter>
-  </Table>
+  <table>
+    <thead>
+      <tr>
+        <th>Header 1</th>
+        <th>Header 2</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Data 1</td>
+        <td>Data 2</td>
+      </tr>
+      <tr>
+        <td>More Data 1</td>
+        <td>More Data 2</td>
+      </tr>
+    </tbody>
+    <tfoot>
+      <tr>
+        <td colspan="2">Footer Information</td>
+      </tr>
+    </tfoot>
+  </table>
 `;
-
-const overviewText = `
-Rad- og kolonneoverskrifter er viktige. En celle fungerer som overskrift hvis den beskriver innholdet i cellene under eller ved siden av. Tall bør høyrejusteres for enklere sammenligning.
-`;
-
-export const Statistics = () => (
-	<div>
-		<SSBTable caption="Antall besøk og årsverk på museer og samlinger">
-			<TableHead>
-				<TableRow>
-					<TableCell type="th" colSpan={1} />
-					<TableCell type="th" colSpan={3} align="center">
-						Antall
-					</TableCell>
-				</TableRow>
-				<TableRow>
-					<TableCell type="th">Museer og samlinger totalt</TableCell>
-					<TableCell type="th" align="center">
-						2020
-					</TableCell>
-					<TableCell type="th" align="center">
-						2021
-					</TableCell>
-					<TableCell type="th" align="center">
-						2022
-					</TableCell>
-				</TableRow>
-			</TableHead>
-			<TableBody>
-				<TableRow>
-					<TableCell type="th" scope="row">
-						Museer og samlinger totalt<sup>1</sup>
-					</TableCell>
-					<TableCell align="center">102</TableCell>
-					<TableCell align="center">101</TableCell>
-					<TableCell align="center">104</TableCell>
-				</TableRow>
-				<TableRow>
-					<TableCell type="th" scope="row" indentationLevel={1}>
-						Besøk totalt
-					</TableCell>
-					<TableCell align="center" style={{ fontWeight: 'bold' }}>
-						5 134 293
-					</TableCell>
-					<TableCell align="center" style={{ fontWeight: 'bold' }}>
-						6 485 173
-					</TableCell>
-					<TableCell align="center" style={{ fontWeight: 'bold' }}>
-						10 321 374
-					</TableCell>
-				</TableRow>
-				<TableRow>
-					<TableCell type="th" scope="row" indentationLevel={2}>
-						Enkeltbesøk
-					</TableCell>
-					<TableCell align="center">4 421 942</TableCell>
-					<TableCell align="center">5 474 120</TableCell>
-					<TableCell align="center">8 491 379</TableCell>
-				</TableRow>
-				<TableRow>
-					<TableCell type="th" scope="row" indentationLevel={2}>
-						Gruppebesøk
-					</TableCell>
-					<TableCell align="center">712 351</TableCell>
-					<TableCell align="center">1 011 053</TableCell>
-					<TableCell align="center">1 829 995</TableCell>
-				</TableRow>
-				<TableRow>
-					<TableCell type="th" scope="row" indentationLevel={2}>
-						Betalende besøkende
-					</TableCell>
-					<TableCell align="center">2 391 962</TableCell>
-					<TableCell align="center">2 774 376</TableCell>
-					<TableCell align="center">5 654 919</TableCell>
-				</TableRow>
-				<TableRow>
-					<TableCell type="th" scope="row" indentationLevel={1}>
-						Årsverk
-					</TableCell>
-					<TableCell align="center" />
-					<TableCell align="center" />
-					<TableCell align="center" />
-				</TableRow>
-				<TableRow>
-					<TableCell type="th" scope="row" indentationLevel={2}>
-						Lønte
-					</TableCell>
-					<TableCell align="center">4 173</TableCell>
-					<TableCell align="center">4 404</TableCell>
-					<TableCell align="center">4 645</TableCell>
-				</TableRow>
-				<TableRow>
-					<TableCell type="th" scope="row" indentationLevel={2}>
-						Faste stillinger
-					</TableCell>
-					<TableCell align="center">3 359</TableCell>
-					<TableCell align="center">3 516</TableCell>
-					<TableCell align="center">3 683</TableCell>
-				</TableRow>
-			</TableBody>
-			<TableFooter>
-				<TableRow>
-					<TableCell type="td" colSpan={4}>
-						<div>
-							<sup>1</sup> Tallet på museer vil variere fra år til år. For
-							eksempel leverer noen museer tall til statistikken ett år, men
-							ikke et annet.
-						</div>
-					</TableCell>
-				</TableRow>
-			</TableFooter>
-		</SSBTable>
-	</div>
-);
 
 const TableInfo = () => {
-	const [activeTab, changeTab] = useState(tabItems[0].path);
-	const [activeCodeTab, changeCodeTab] = useState(tabCode[0].path);
+	const [activeTab, setActiveTab] = useState({
+		contentTab: tabItems[0].path,
+		codeTab: tabCode[0].path,
+	});
 
-	const tabClicked = newTabPath => changeTab(newTabPath);
-	const tabCodeClicked = newTabPath => changeCodeTab(newTabPath);
+	const handleTabClick = (newTabPath, tabType) => {
+		setActiveTab(prevState => ({
+			...prevState,
+			[tabType]: newTabPath,
+		}));
+	};
 
 	return (
 		<div className="col-lg-12">
@@ -226,14 +216,14 @@ const TableInfo = () => {
 				ikke inneholder mer enn ett interaktivt element.
 			</LeadParagraph>
 			<Tabs
-				activeOnInit={tabItems[0].path}
+				activeOnInit={activeTab.contentTab}
 				items={tabItems}
-				onClick={tabClicked}
+				onClick={newTabPath => handleTabClick(newTabPath, 'contentTab')}
 			/>
 
 			<Divider className="mb-4" />
 
-			{activeTab === '/overview' && (
+			{activeTab.contentTab === '/overview' && (
 				<div className="row mb-4">
 					<Title size={2} className="col-lg-12">
 						Retningslinjer for tabell
@@ -247,22 +237,22 @@ const TableInfo = () => {
 					<div className="component-example col-lg col-md-12 divider-left" />
 					<div className="col-lg-12">
 						<Tabs
-							activeOnInit={tabCode[0].path}
+							activeOnInit={activeTab.codeTab}
 							items={tabCode}
-							onClick={tabCodeClicked}
+							onClick={newTabPath => handleTabClick(newTabPath, 'codeTab')}
 						/>
 						<Divider light />
-						{activeCodeTab === '/react' && (
+						{activeTab.codeTab === '/react' && (
 							<CodeSnippet code={codeReact} language="jsx" />
 						)}
-						{activeCodeTab === '/html' && (
+						{activeTab.codeTab === '/html' && (
 							<CodeSnippet code={codeHtml} language="html" />
 						)}
 					</div>
 				</div>
 			)}
 
-			{activeTab === '/props' && (
+			{activeTab.contentTab === '/props' && (
 				<div>
 					<div className="row col-lg-12">
 						<table className="col-lg-12 mb-4">
@@ -280,7 +270,6 @@ const TableInfo = () => {
 								</tr>
 							</thead>
 							<tbody>
-								{/* TableElementProps */}
 								<tr>
 									<td style={{ paddingLeft: '20px' }}>
 										<code>className</code>

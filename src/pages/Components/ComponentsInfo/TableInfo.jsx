2a7c08c388fc270a1f4e1ffba93d745c70031ab8
Carl-OW <142233642+Carl-OW@users.noreply.github.com>
2024-08-29 08:09:53 +0200

    Cleaner Code

--- src/pages/Components/ComponentsInfo/TableInfo.jsx
+++ src/pages/Components/ComponentsInfo/TableInfo.jsx
@@ -9,9 +9,11 @@ import {
 } from '@statisticsnorway/ssb-component-library';
 import {
 	overviewText,
+	LeadText,
 	TableExample,
 	codeReact,
 	codeHtml,
+	PropsTableComponent,
 } from './TableInfoContent';
 
 const tabItems = [
@@ -40,13 +42,7 @@ const TableInfo = () => {
 	return (
 		<div className="col-lg-12">
 			<Title size={1}>Table</Title>
-			<LeadParagraph>
-				Tabeller brukes til å organisere og vise data på en strukturert måte.
-				Ulike skjermlesere håndterer tabeller litt forskjellig, men felles er at
-				bare en liten del av tabellen kan presenteres av gangen: ofte bare én
-				celle. Det er en fordel at tabellceller ikke slås sammen, og at cellene
-				ikke inneholder mer enn ett interaktivt element.
-			</LeadParagraph>
+			<LeadParagraph>{LeadText}</LeadParagraph>
 			<Tabs
 				activeOnInit={activeTab.contentTab}
 				items={tabItems}
@@ -75,10 +71,10 @@ const TableInfo = () => {
 						/>
 						<Divider light />
 						{activeTab.codeTab === '/react' && (
-							<CodeSnippet code={codeReact} language="jsx" />
+							<CodeSnippet code={codeReact || ''} language="jsx" />
 						)}
 						{activeTab.codeTab === '/html' && (
-							<CodeSnippet code={codeHtml} language="html" />
+							<CodeSnippet code={codeHtml || ''} language="html" />
 						)}
 					</div>
 				</div>
@@ -86,184 +82,7 @@ const TableInfo = () => {
 
 			{activeTab.contentTab === '/props' && (
 				<div>
-					<div className="row col-lg-12">
-						<table className="col-lg-12 mb-4">
-							<thead style={{ textAlign: 'left' }}>
-								<tr>
-									<th>
-										<Title size={3}>TableElementProps</Title>
-									</th>
-									<th>
-										<Title size={3}>Type</Title>
-									</th>
-									<th>
-										<Title size={3}>Description</Title>
-									</th>
-								</tr>
-							</thead>
-							<tbody>
-								<tr>
-									<td style={{ paddingLeft: '20px' }}>
-										<code>className</code>
-									</td>
-									<td>string</td>
-									<td>Optional container class</td>
-								</tr>
-								<tr>
-									<td style={{ paddingLeft: '20px' }}>
-										<code>children</code>
-									</td>
-									<td>ReactNode</td>
-									<td>Content to be displayed within the component</td>
-								</tr>
-							</tbody>
-						</table>
-
-						<Divider className="mb-4" light />
-
-						<table className="col-lg-12 mb-4">
-							<thead style={{ textAlign: 'left' }}>
-								<tr>
-									<th>
-										<Title size={3}>TableProps</Title>
-									</th>
-									<th>
-										<Title size={3}>Type</Title>
-									</th>
-									<th>
-										<Title size={3}>Description</Title>
-									</th>
-								</tr>
-							</thead>
-							<tbody>
-								<tr>
-									<td style={{ paddingLeft: '20px' }}>
-										<code>caption</code>
-									</td>
-									<td>string</td>
-									<td>Optional caption for the table</td>
-								</tr>
-								<tr>
-									<td style={{ paddingLeft: '20px' }}>
-										<code>dataNoteRefs</code>
-									</td>
-									<td>string</td>
-									<td>References to data notes</td>
-								</tr>
-								<tr>
-									<td style={{ paddingLeft: '20px' }}>
-										<code>className</code>
-									</td>
-									<td>string</td>
-									<td>Optional container class</td>
-								</tr>
-								<tr>
-									<td style={{ paddingLeft: '20px' }}>
-										<code>children</code>
-									</td>
-									<td>ReactNode</td>
-									<td>Content to be displayed within the component</td>
-								</tr>
-							</tbody>
-						</table>
-
-						<Divider className="mb-4" light />
-
-						<table className="col-lg-12 mb-4">
-							<thead style={{ textAlign: 'left' }}>
-								<tr>
-									<th>
-										<Title size={3}>TableCellProps</Title>
-									</th>
-									<th>
-										<Title size={3}>Type</Title>
-									</th>
-									<th>
-										<Title size={3}>Description</Title>
-									</th>
-								</tr>
-							</thead>
-							<tbody>
-								<tr>
-									<td style={{ paddingLeft: '20px' }}>
-										<code>id</code>
-									</td>
-									<td>string</td>
-									<td>Unique identifier for the table cell</td>
-								</tr>
-								<tr>
-									<td style={{ paddingLeft: '20px' }}>
-										<code>className</code>
-									</td>
-									<td>string</td>
-									<td>Optional container class</td>
-								</tr>
-								<tr>
-									<td style={{ paddingLeft: '20px' }}>
-										<code>type</code>
-									</td>
-									<td>&apos;th&apos; | &apos;td&apos;</td>
-									<td>
-										Specifies whether the cell is a header or standard cell
-									</td>
-								</tr>
-								<tr>
-									<td style={{ paddingLeft: '20px' }}>
-										<code>rowSpan</code>
-									</td>
-									<td>number</td>
-									<td>Number of rows the cell should span</td>
-								</tr>
-								<tr>
-									<td style={{ paddingLeft: '20px' }}>
-										<code>colSpan</code>
-									</td>
-									<td>number</td>
-									<td>Number of columns the cell should span</td>
-								</tr>
-								<tr>
-									<td style={{ paddingLeft: '20px' }}>
-										<code>headers</code>
-									</td>
-									<td>string</td>
-									<td>IDs of header cells this cell is related to</td>
-								</tr>
-								<tr>
-									<td style={{ paddingLeft: '20px' }}>
-										<code>scope</code>
-									</td>
-									<td>
-										&apos;col&apos; | &apos;colgroup&apos; | &apos;row&apos; |
-										&apos;rowgroup&apos;
-									</td>
-									<td>Specifies the scope of the header cell</td>
-								</tr>
-								<tr>
-									<td style={{ paddingLeft: '20px' }}>
-										<code>align</code>
-									</td>
-									<td>
-										&apos;left&apos; | &apos;center&apos; | &apos;right&apos;
-									</td>
-									<td>Text alignment within the cell</td>
-								</tr>
-								<tr>
-									<td style={{ paddingLeft: '20px' }}>
-										<code>indentationLevel</code>
-									</td>
-									<td>&apos;1&apos; | &apos;2&apos; | &apos;3&apos;</td>
-									<td>Specifies the level of indentation</td>
-								</tr>
-								<tr>
-									<td style={{ paddingLeft: '20px' }}>
-										<code>children</code>
-									</td>
-									<td>ReactNode | string | number</td>
-									<td>Content to be displayed within the cell</td>
-								</tr>
-							</tbody>
-						</table>
-					</div>
+					<PropsTableComponent />
 				</div>
 			)}
 		</div>
--- src/pages/Components/ComponentsInfo/TableInfoContent.jsx
+++ src/pages/Components/ComponentsInfo/TableInfoContent.jsx
@@ -6,45 +6,274 @@ import {
 	TableFooter,
 	TableRow,
 	TableCell,
+	Title,
+	Divider,
 } from '@statisticsnorway/ssb-component-library';
+import PropTypes from 'prop-types';
 
 export const overviewText = 'Rad- og kolonneoverskrifter er essensielle. En celle er overskrift hvis den beskriver innholdet i cellene under eller ved siden av. Juster tall for enkel sammenligning.';
+
+export const LeadText = `
+	Tabeller brukes til å organisere og vise data på en strukturert måte.
+	Ulike skjermlesere håndterer tabeller litt forskjellig, men felles er at
+	bare en liten del av tabellen kan presenteres av gangen: ofte bare én
+	celle. Det er en fordel at tabellceller ikke slås sammen, og at cellene
+	ikke inneholder mer enn ett interaktivt element.
+`;
 
 export const tableData = [
 	{
 		id: 'museer_totalt',
 		header: 'Museer og samlinger totalt',
-		data: ['102', '101', '104'],
+		data: { 2020: '102', 2021: '101', 2022: '104' },
 	},
 	{
 		id: 'besok_totalt',
 		header: 'Besøk totalt',
-		data: ['5 134 293', '6 485 173', '10 321 374'],
+		data: { 2020: '5 134 293', 2021: '6 485 173', 2022: '10 321 374' },
 		bold: true,
 	},
 	{
 		id: 'enkeltbesok',
 		header: 'Enkeltbesøk',
-		data: ['4 421 942', '5 474 120', '8 491 379'],
+		data: { 2020: '4 421 942', 2021: '5 474 120', 2022: '8 491 379' },
 	},
 	{
 		id: 'gruppebesok',
 		header: 'Gruppebesøk',
-		data: ['712 351', '1 011 053', '1 829 995'],
+		data: { 2020: '712 351', 2021: '1 011 053', 2022: '1 829 995' },
 	},
 	{
 		id: 'betalende',
 		header: 'Betalende besøkende',
-		data: ['2 391 962', '2 774 376', '5 654 919'],
+		data: { 2020: '2 391 962', 2021: '2 774 376', 2022: '5 654 919' },
 	},
-	{ id: 'arsverk', header: 'Årsverk', data: ['', '', ''] },
-	{ id: 'lonte', header: 'Lønte', data: ['4173', '4404', '4645'] },
+	{
+		id: 'arsverk',
+		header: 'Årsverk',
+		data: { 2020: '', 2021: '', 2022: '' },
+	},
+	{
+		id: 'lonte',
+		header: 'Lønte',
+		data: { 2020: '4173', 2021: '4404', 2022: '4645' },
+	},
 	{
 		id: 'faste_stillinger',
 		header: 'Faste stillinger',
-		data: ['3359', '3516', '3683'],
+		data: { 2020: '3359', 2021: '3516', 2022: '3683' },
 	},
 ];
+
+export const TableExample = () => {
+	const years = Object.keys(tableData[0].data);
+	const boldStyle = { fontWeight: 'bold' };
+
+	return (
+		<SSBTable caption="Antall besøk og årsverk på museer og samlinger">
+			<TableHead>
+				<TableRow>
+					<TableCell type="th" colSpan={1} />
+					<TableCell type="th" colSpan={years.length} align="center">
+						Antall
+					</TableCell>
+				</TableRow>
+				<TableRow>
+					<TableCell type="th">Museer og samlinger totalt</TableCell>
+					{years.map(year => (
+						<TableCell type="th" align="right" key={`year_${year}`}>
+							{year}
+						</TableCell>
+					))}
+				</TableRow>
+			</TableHead>
+			<TableBody>
+				{tableData.map(({ id, header, data, bold }) => (
+					<TableRow key={id}>
+						<TableCell type="th" scope="row">
+							{header}
+						</TableCell>
+						{years.map(year => (
+							<TableCell
+								align="right"
+								style={bold ? boldStyle : {}}
+								key={`${id}_${year}`}
+							>
+								{data[year]}
+							</TableCell>
+						))}
+					</TableRow>
+				))}
+			</TableBody>
+			<TableFooter>
+				<TableRow>
+					<TableCell type="td" colSpan={years.length + 1}>
+						<div>
+							<sup>1</sup> Tallet på museer vil variere fra år til år. For
+							eksempel leverer noen museer tall til statistikken ett år, men
+							ikke et annet.
+						</div>
+					</TableCell>
+				</TableRow>
+			</TableFooter>
+		</SSBTable>
+	);
+};
+
+const propPageData = [
+	{
+		title: 'TableElementProps',
+		props: [
+			{
+				name: 'className',
+				type: 'string',
+				description: 'Optional container class',
+			},
+			{
+				name: 'children',
+				type: 'ReactNode',
+				description: 'Content to be displayed within the component',
+			},
+		],
+	},
+	{
+		title: 'TableProps',
+		props: [
+			{
+				name: 'caption',
+				type: 'string',
+				description: 'Optional caption for the table',
+			},
+			{
+				name: 'dataNoteRefs',
+				type: 'string',
+				description: 'References to data notes',
+			},
+			{
+				name: 'className',
+				type: 'string',
+				description: 'Optional container class',
+			},
+			{
+				name: 'children',
+				type: 'ReactNode',
+				description: 'Content to be displayed within the component',
+			},
+		],
+	},
+	{
+		title: 'TableCellProps',
+		props: [
+			{
+				name: 'id',
+				type: 'string',
+				description: 'Unique identifier for the table cell',
+			},
+			{
+				name: 'className',
+				type: 'string',
+				description: 'Optional container class',
+			},
+			{
+				name: 'type',
+				type: "'th' | 'td'",
+				description: 'Specifies whether the cell is a header or standard cell',
+			},
+			{
+				name: 'rowSpan',
+				type: 'number',
+				description: 'Number of rows the cell should span',
+			},
+			{
+				name: 'colSpan',
+				type: 'number',
+				description: 'Number of columns the cell should span',
+			},
+			{
+				name: 'headers',
+				type: 'string',
+				description: 'IDs of header cells this cell is related to',
+			},
+			{
+				name: 'scope',
+				type: "'col' | 'colgroup' | 'row' | 'rowgroup'",
+				description: 'Specifies the scope of the header cell',
+			},
+			{
+				name: 'align',
+				type: "'left' | 'center' | 'right'",
+				description: 'Text alignment within the cell',
+			},
+			{
+				name: 'indentationLevel',
+				type: "'1' | '2' | '3'",
+				description: 'Specifies the level of indentation',
+			},
+			{
+				name: 'children',
+				type: 'ReactNode | string | number',
+				description: 'Content to be displayed within the cell',
+			},
+		],
+	},
+];
+
+const PropsTable = ({ data }) => (
+	<div className="row col-lg-12">
+		{data.map((section, index) => (
+			<React.Fragment key={section.title}>
+				{index > 0 && <Divider className="mb-4" light />}
+				<table className="col-lg-12 mb-4">
+					<thead style={{ textAlign: 'left' }}>
+						<tr>
+							<th>
+								<Title size={3}>{section.title}</Title>
+							</th>
+							<th>
+								<Title size={3}>Type</Title>
+							</th>
+							<th>
+								<Title size={3}>Description</Title>
+							</th>
+						</tr>
+					</thead>
+					<tbody>
+						{section.props.map(prop => (
+							<tr key={prop.name}>
+								<td style={{ paddingLeft: '20px' }}>
+									<code>{prop.name}</code>
+								</td>
+								<td>{prop.type}</td>
+								<td>{prop.description}</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			</React.Fragment>
+		))}
+	</div>
+);
+
+PropsTable.propTypes = {
+	data: PropTypes.arrayOf(
+		PropTypes.shape({
+			title: PropTypes.string.isRequired,
+			props: PropTypes.arrayOf(
+				PropTypes.shape({
+					name: PropTypes.string.isRequired,
+					type: PropTypes.string.isRequired,
+					description: PropTypes.string.isRequired,
+				}),
+			).isRequired,
+		}),
+	).isRequired,
+};
+
+export const PropsTableComponent = () => (
+	<div>
+		<PropsTable data={propPageData} />
+	</div>
+);
 
 export const codeReact = `
 import { Table, TableHead, TableBody, TableFooter, TableRow, TableCell } from "@statisticsnorway/ssb-component-library";
@@ -103,53 +332,3 @@ export const codeHtml = `
     </tfoot>
   </table>
 `;
-
-export const TableExample = () => (
-	<SSBTable caption="Antall besøk og årsverk på museer og samlinger">
-		<TableHead>
-			<TableRow>
-				<TableCell type="th" colSpan={1} />
-				<TableCell type="th" colSpan={3} align="center">
-					Antall
-				</TableCell>
-			</TableRow>
-			<TableRow>
-				<TableCell type="th">Museer og samlinger totalt</TableCell>
-				{['2020', '2021', '2022'].map(year => (
-					<TableCell type="th" align="right" key={`year_${year}`}>
-						{year}
-					</TableCell>
-				))}
-			</TableRow>
-		</TableHead>
-		<TableBody>
-			{tableData.map(row => (
-				<TableRow key={row.id}>
-					<TableCell type="th" scope="row">
-						{row.header}
-					</TableCell>
-					{row.data.map(item => (
-						<TableCell
-							align="right"
-							style={row.bold ? { fontWeight: 'bold' } : {}}
-							key={`${row.id}_${item}`}
-						>
-							{item}
-						</TableCell>
-					))}
-				</TableRow>
-			))}
-		</TableBody>
-		<TableFooter>
-			<TableRow>
-				<TableCell type="td" colSpan={4}>
-					<div>
-						<sup>1</sup> Tallet på museer vil variere fra år til år. For
-						eksempel leverer noen museer tall til statistikken ett år, men ikke
-						et annet.
-					</div>
-				</TableCell>
-			</TableRow>
-		</TableFooter>
-	</SSBTable>
-);
